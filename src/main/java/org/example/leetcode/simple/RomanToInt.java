10e451b8e390c9319d185c42a4f5910e9022843a
caopingzhi <17670844466@163.com>
2023-06-10 15:28:11 +0800

    罗马数字转整形

---
+++ src/main/java/org/example/leetcode/simple/RomanToInt.java
@@ -0,0 +1,63 @@
+/**
+ * 北京云锦汇智信息技术有限公司 版权所有Copyright (C) 2022
+ *
+ * @ProjectName: leetcode-example
+ * @Package: org.example.leetcode.simple
+ * @ClassName: RomanToInt
+ * @CreateUser: 曹评智
+ * @CreateDate: 2023/6/8 16:57 星期四
+ */
+package org.example.leetcode.simple;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description: 罗马数字转整形
+ * @author 曹评智
+ * @since 2023/6/8 19:57 星期四
+ */
+public class RomanToInt {
+
+    public static void main(String[] args) {
+        String s = "MCMXCIVLVIIIXLV";
+        int res = romanToInt(s);
+        System.out.println(res);
+    }
+
+    public static int romanToInt(String s) {
+        int result = 0;
+        int len = s.length();
+        Map<String, Integer> groupMap = new HashMap<>();
+        groupMap.put("IV", 4);
+        groupMap.put("IX", 9);
+        groupMap.put("XL", 40);
+        groupMap.put("XC", 90);
+        groupMap.put("CD", 400);
+        groupMap.put("CM", 900);
+
+        Map<Character, Integer> scoreMap = new HashMap<>();
+        scoreMap.put('I', 1);
+        scoreMap.put('V', 5);
+        scoreMap.put('X', 10);
+        scoreMap.put('L', 50);
+        scoreMap.put('C', 100);
+        scoreMap.put('D', 500);
+        scoreMap.put('M', 1000);
+        for (int i = 0; i < len; i++) {
+            if (i == len - 1) {
+                result += scoreMap.get(s.charAt(i));
+                continue;
+            }
+            String groupStr = s.substring(i, i + 2);
+            if (groupMap.containsKey(groupStr)) {
+                result += groupMap.get(groupStr);
+                i ++;
+            } else {
+                result += scoreMap.get(s.charAt(i));
+            }
+        }
+        return result;
+    }
+
+}
